a72f5da2d54778424fa9a630e743e8650e8dd2bd
Jackson Shea <jackson.shea.752@my.csun.edu>
2023-10-21 12:50:47 -0700

    all files put into same directory for testing purposes

---
+++ src/trying/basic/ConsultationBooking.sol
@@ -0,0 +1,85 @@
+subl Failed// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ConsultationBooking {
+    address public doctor;
+    address public patient;
+    uint256 public consultationFee;
+    bool public consultationStarted;
+    bool public consultationCompleted;
+    address payable wallet;
+    
+    
+
+    event ConsultationBooked(address indexed patient, uint256 fee);
+    event ConsultationStarted(address indexed doctor, address indexed patient);
+    event ConsultationCompleted(address indexed doctor, address indexed patient);
+
+    modifier onlyDoctor() {
+        require(msg.sender == doctor, "Only the doctor can call this function");
+        _;
+    }  
+
+        
+
+    modifier onlyPatient() {
+        require(msg.sender == patient, "Only the patient can call this function");
+        _;
+    }
+
+    modifier consultationNotStarted() {
+        require(!consultationStarted, "Consultation has already started");
+        _;
+    }
+
+    modifier consultationNotCompleted() {
+        require(!consultationCompleted, "Consultation has already been completed");
+        _;
+    }
+
+
+    constructor(address _patient,address _doctor, uint256 _consultationFee) {
+        require(_doctor != address(0), "Invalid doctor address");
+        require(_consultationFee > 0, "Consultation fee must be greater than 0");
+
+        doctor = _doctor;
+        patient = _patient;
+
+        consultationFee = _consultationFee;
+    }
+
+    function bookConsultaion() external payable onlyPatient consultationNotStarted {
+    require(msg.value == consultationFee, "Incorrect consultation fee");
+
+    // Remove this line as patient is already set in the constructor
+    // patient = msg.sender;
+
+    consultationStarted = true;
+
+    emit ConsultationBooked(patient, consultationFee);
+}
+
+    function startConsultation() external onlyDoctor consultationNotCompleted {
+        require(consultationStarted, "Consultation has not been booked yet");
+
+        emit ConsultationStarted(doctor, patient);
+    }
+
+    function completeConsultation() external onlyDoctor consultationNotCompleted {
+        require(consultationStarted, "Consultation has not been booked yet");
+
+        // Transfer the consultation fee to the doctor
+        (bool success, ) = doctor.call{value: consultationFee}("");
+        require(success, "Failed to transfer consultation fee to the doctor");
+
+        consultationCompleted = true;
+
+        emit ConsultationCompleted(doctor, patient);
+    }
+   
+
+    // Fallback function to reject direct payments to the contract
+    receive() external payable {
+        revert("Use the bookConsultation function to initiate the consultation");
+    }
+}
---
+++ src/trying/basic/FileIO.java
@@ -0,0 +1,65 @@
+package trying;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+public class FileIO {
+	
+	public String[] doc = new String[5];
+    public String[] patient = new String[5];
+
+    public static void main(String[] args) {
+        try {
+            //String filePath = "C:/Users/pd681898/Downloads/Data.zip/Data/Clean Transcripts/CAR0001.txt";
+            BufferedReader reader = new BufferedReader(new FileReader("CAR0001.txt"));
+            
+            String[] doc = new String[5];
+            String[] patient = new String[5];
+            
+            String line;
+            char firstChar;
+            
+            int d=0;
+            int p=0;
+
+           
+            while ((line = reader.readLine()) != null) {
+                if (!line.isEmpty()) {  // Check for empty lines
+                    firstChar = line.charAt(0);
+
+                    if (firstChar == 'P') {
+                       patient[p] = line ;
+                       System.out.println(line);
+                       if(p ==4) break;
+                       p++;
+                       // Exit the loop if 'P' is found
+                    }
+                    
+                    else {
+                    	
+                    	doc[d] = line;
+                    	System.out.println(line);
+                    	if(d ==4) break;
+                    	d++;
+                     }
+
+                   
+                }
+                
+            }
+            
+            
+            
+          
+            
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
+        }
+    }
+    
+   }
