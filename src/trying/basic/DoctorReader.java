0cfed697e62886bcc29dc90fc1696ae868954ec4
Desai <param-ridham.desai.167@my.csun.edu>
2023-12-11 17:04:33 -0800

    implementing the data from patient in patientredear class

---
+++ src/trying/basic/DoctorReader.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class DoctorReader {
+
+    public static String[][] docs = new String[15][5];
+
+    public static String[][] getDoctorMessages() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("CAR0001.txt"));
+            String line;
+            char firstChar;
+            int d = 0, i = 0, j = 0, k = 0;
+
+            while ((line = reader.readLine()) != null) {
+                if (!line.isEmpty()) {
+                    firstChar = line.charAt(0);
+
+                    if (firstChar == 'D') {
+                        if (d >= 5 && d < 80) {
+                            if (d % 5 == 0 && d != 0) {
+                                k++;
+                                j = 0;
+                            }
+                            if (k < 15 && j < 5) { // Ensure indices are within bounds
+                                docs[k][j] = line;
+                                j++;
+                                d++;
+                            } else {
+                                break;
+                            }
+                        } else {
+                            if (d < 15 && i < 5) { // Ensure indices are within bounds
+                                docs[d][i] = line;
+                                i++;
+                                d++;
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return docs;
+    }
+
+    public static void main(String[] args) {
+        String[][] result = getDoctorMessages();
+
+        // Do something with the result if needed
+        for (String[] doctorArray : result) {
+            for (String message : doctorArray) {
+                if (message != null) {
+                    System.out.println(message);
+                }
+            }
+        }
+    }
+}
---
+++ src/trying/basic/PatientReader.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class PatientReader {
+
+    public static String[][] patient = new String[15][5];
+
+    public static String[][] getPatientMessages() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("CAR0001.txt"));
+            String line;
+            char firstChar;
+            int p= 0, i = 0, j = 0, k = 0;
+
+            while ((line = reader.readLine()) != null) {
+                if (!line.isEmpty()) {
+                    firstChar = line.charAt(0);
+
+                    if (firstChar == 'P') {
+                        if (p >= 5 && p < 80) {
+                            if (p % 5 == 0 && p != 0) {
+                                k++;
+                                j = 0;
+                            }
+                            if (k < 15 && j < 5) { // Ensure indices are within bounds
+                                patient[k][j] = line;
+                                j++;
+                                p++;
+                            } else {
+                                break;
+                            }
+                        } else {
+                            if (p < 15 && i < 5) { // Ensure indices are within bounds
+                                patient[p][i] = line;
+                                i++;
+                                p++;
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return patient;
+    }
+
+    public static void main(String[] args) {
+        String[][] result = getPatientMessages();
+
+        // Do something with the result if needed
+        for (String[] PatientArray : result) {
+            for (String message : PatientArray) {
+                if (message != null) {
+                    System.out.println(message);
+                }
+            }
+        }
+    }
+}
